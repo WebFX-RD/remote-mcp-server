b2dffc40d080879d589d01183399890d9b78e441
kamal <kamal@webfx.com>
2025-11-25 20:54:45 -0500

    use direct VPC to access mongo

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,6 @@ gcloud run deploy remote-mcp-server \
 --timeout=3600 \
 --set-build-env-vars=NPM_TOKEN=${NPM_TOKEN} \
 --set-cloudsql-instances=idyllic-vehicle-159522:us-east1:mcfx,idyllic-vehicle-159522:us-central1:mcfx-revops \
---set-env-vars=LOCALDOMAIN=c.idyllic-vehicle-159522.internal
+--set-env-vars=LOCALDOMAIN=c.idyllic-vehicle-159522.internal \
+--network=default \
+--subnet=default
